817a218f2879f1b70aefc1903be2ac6bbac66f07
Izbits00 <mindtech.mchn@gmail.com>
2025-03-22 11:21:22 +0700

    add login page not finnish

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
+const app = document.getElementById('app');
+
+async function loadLogin() {
+  const res = await fetch('./components/login.html');
+  const html = await res.text();
+  app.innerHTML = html;
+
+  // Optional: bind logic after loading
+  const form = app.querySelector('#loginForm');
+  if (form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const username = form.username.value;
+      const password = form.password.value;
+
+      if (username === 'admin' && password === '1234') {
+        alert('Login successful!');
+      } else {
+        alert('Invalid credentials');
+      }
+    });
+  }
+}
+
 function helloWorld () {
   return "Hellow Holiday"
 }
 
-export { helloWorld }
+export { helloWorld, loadLogin }
 
