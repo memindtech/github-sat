51bfc2073a65ad44e8985f7816d287b0bd76002b
Izbits00 <mindtech.mchn@gmail.com>
2025-03-22 12:59:20 +0700

    Revert "Merge pull request #1 from memindtech/feature/login"
    This reverts commit aeeeac31db4c55f8046dd789035793a8925f5daa, reversing
    changes made to 228cea70d2d7af17d695ae20d47fcf70c4a6caea.

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-const app = document.getElementById('app');
-
-async function loadLogin() {
-  const res = await fetch('./components/login.html');
-  const html = await res.text();
-  app.innerHTML = html;
-
-  // Optional: bind logic after loading
-  const form = app.querySelector('#loginForm');
-  if (form) {
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const username = form.username.value;
-      const password = form.password.value;
-
-      if (username === 'admin' && password === '1234') {
-        alert('Login successful!');
-      } else {
-        alert('Invalid credentials');
-      }
-    });
-  }
-}
-
 function helloWorld () {
   return "Hello Holiday"
 }
@@ -30,4 +6,5 @@ function secondFunction (text) {
   return text;
 }
 
-export { helloWorld, loadLogin, secondFunction }
+export { helloWorld, secondFunction }
+
